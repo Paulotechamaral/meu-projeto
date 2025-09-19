bed8690f65a7e1ea049bfa700b71b7edc65bb3cb
Paulotechamaral <paulovictorgomes95@gmail.com>
2025-09-19 00:10:24 -0300

    atualização vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "jsdom",  // <-- importante para testes que usam DOM
     globals: true,
-    setupFiles: "./vitest.setup.ts", // se precisar de setup adicional
-  },
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts"
+  }
 });
