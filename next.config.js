b67993b8ddb1b8df4b53c7e6885519312d4a2340
Paulotechamaral <paulovictorgomes95@gmail.com>
2025-09-21 13:14:03 -0300

    atualização 20

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // 👈 desativa Image Optimization
   },
+  basePath: "/meu-projeto",
+  assetPrefix: "/meu-projeto/",
 };
 
 export default nextConfig;
